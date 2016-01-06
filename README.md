d005aaca37676665382b18f97e436c80c2860f79
shivin9 <shivin.srivastava@gmail.com>
2016-01-06 20:30:39 +0530

    modified:   README.md 	modified:   corpora/book 	modified:   corpora/clean_titles.py 	new file:   corpora/new 	deleted:    corpora/book~ 	deleted:    corpora/clean_titles.py~

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # will-your-essay-make-it-
+
+cleaned the book, now we have 50 essays
--- corpora/clean_titles.py
+++ corpora/clean_titles.py
@@ -1,5 +1,6 @@
 import re
 f = open('book','rw')
-str = f.read()
-re.sub(r".*[\x90-\xff].*", str)
-f.write(str)
+str1 = f.read()
+str1 = re.sub(r".*[\x90-\xff].*", "",str1)
+print str1
+
